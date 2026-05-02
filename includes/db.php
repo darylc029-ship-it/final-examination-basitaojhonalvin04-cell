6f94983128e494ea1fe038b44a14fe678a91430b
darylc029-ship-it <darylc029@gmail.com>
2026-05-02 17:19:51 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,25 +1,22 @@
 <?php
 $host = '127.0.0.1'; 
-$db   = 'cc111smsdb';  //sukatan yo dytoy jay nagan ti database yo
-$user = 'root';        //
-$pass = '';            //
-$port = '3308';        //nu nagusar kayo sabali nga port sukatan yo metlang dytoy
+$db   = 'cc111smsdb';  
+$user = 'root';        
+$pass = '';            
+$port = '3308';        
 $charset = 'utf8mb4';
-
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // show errors
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // fetch as assoc array
+    PDO::ATTR_EMULATE_PREPARES   => false,                  // real prepared statements
 ];
 
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-
-} catch (\PDOException $e) {
-
-     die("Connection failed: " . $e->getMessage());
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    die("Database connection failed.");
 }
 ?>
